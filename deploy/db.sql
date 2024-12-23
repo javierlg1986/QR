a213899416e2471f6ae8be19049186fe1a284ef7
javierlg1986 <javierlg1986@gmail.com>
2024-12-23 11:13:26 +0100

    archivos deploy base

---
+++ deploy/db.sql
@@ -0,0 +1,55 @@
+CREATE TABLE usuarios (
+    id INTEGER PRIMARY KEY,
+    usuario VARCHAR(254) PRIMARY KEY,
+    administrador INTEGER, 
+    nombre_completo VARCHAR(254),
+    pass VARCHAR(24),
+    bloqueado INTEGER,
+    permiso INTEGER, 
+    diestro INTEGER,
+    token VARCHAR(8)
+);
+
+CREATE TABLE log_accesos (
+    id INTEGER PRIMARY KEY,
+    fecha INTEGER,
+    id_usuario INTEGER,
+    url TEXT,
+    exito BOOLEAN,
+    ip TEXT,
+    FOREIGN KEY(id_usuario) REFERENCES usuarios(id)
+);
+
+CREATE TABLE log_cambios (
+    id INTEGER PRIMARY KEY,
+    id_usuario INTEGER,
+    fecha INTEGER,
+    tabla VARCHAR(254),
+    id_tabla INTEGER,
+    tipo VARCHAR(2),
+    cambio TEXT,
+    FOREIGN KEY(id_usuario) REFERENCES usuarios(id)
+);
+
+CREATE TABLE parameters (
+    id INTEGER PRIMARY KEY,
+    label VARCHAR(254),
+    family VARCHAR(254)
+);
+
+-- CREATE TABLE traballos (
+--     id INTEGER PRIMARY KEY,
+--     expediente VARCHAR(24),
+--     cliente VARCHAR(254),
+--     enderezo VARCHAR(254),
+--     poboacion VARCHAR(254),
+--     latitud FLOAT,
+--     longitud FLOAT,
+--     fecha TIMESTAMP,
+--     observacions_int TEXT,
+--     observacions_ext TEXT,
+--     id_usuario INTEGER,
+--     id_estado INTEGER DEFAULT 1,
+--     FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+--     FOREIGN KEY (id_estado) REFERENCES parameters(id)
+-- );
---
+++ deploy/populate.sql
@@ -0,0 +1,8 @@
+INSERT INTO usuarios (usuario, administrador, nombre_completo, pass, bloqueado, permiso, diestro, token) VALUES ('Admin', '', 'Administrador', '9531a2e3ef77001e6e64a55921399da065092bdd', 0, 2, '', '');
+INSERT INTO usuarios (usuario, administrador, nombre_completo, pass, bloqueado, permiso, diestro, token) VALUES ('JLG', '', 'Javier López García', '', 0, 2, '9531a2e3ef77001e6e64a55921399da065092bdd', '');
+INSERT INTO usuarios (usuario, administrador, nombre_completo, pass, bloqueado, permiso, diestro, token) VALUES ('MVS', '', 'Miriam Varela Sánchez', '9531a2e3ef77001e6e64a55921399da065092bdd', 0, 2, '', '');
+INSERT INTO usuarios (usuario, administrador, nombre_completo, pass, bloqueado, permiso, diestro, token) VALUES ('JGV', '', 'José Gómez Vázquez', '9531a2e3ef77001e6e64a55921399da065092bdd', 0, 2, '', '');
+INSERT INTO usuarios (usuario, administrador, nombre_completo, pass, bloqueado, permiso, diestro, token) VALUES ('ANP', '', 'Adrián Nieto Parente', '9531a2e3ef77001e6e64a55921399da065092bdd', 0, 2, '', '');
+
+-- INSERT INTO parameters (label, family) VALUES ("Pendente información", "estado");
+
